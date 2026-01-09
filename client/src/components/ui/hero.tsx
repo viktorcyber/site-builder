08a894dc70f46b57bde09891810e5b13957789fe
viktorcyber <zin.it.dev@gmail.com>
2026-01-09 15:40:04 +0700

    🔥 CI/CD

--- client/src/components/ui/hero.tsx
+++ client/src/components/ui/hero.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { Link } from 'react-router';
 
 const Hero = () => {
-  const [input, setInput] = useState('');
+  const [, setInput] = useState('');
   const [loading, setLoading] = useState(false);
 
   const onSubmitHandler = async (e: React.FormEvent) => {
